19e5c134673cb91abe0380b746706132d4509287
michael-ugochukwu-ai <michael.ugochukwu@appliedintuition.co>
2025-03-17 13:47:13 -0700

    add business logic

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import os
 from jira import JIRA
+from jira.resources import Issue
 
 def main():
     jira_user = os.environ["JIRA_USER"]
     jira_url = os.environ["JIRA_URL"]
     jira_key = os.environ["JIRA_TICKET_KEY"]
     jira_token = os.environ["JIRA_TOKEN"]
-
-    jira_client= JIRA(server=jira_url, auth=(jira_user, jira_token))
-
+    jira_client= JIRA(server=jira_url, basic_auth=(jira_user, jira_token))
     issue = jira_client.search_issues(f"key = '{jira_key}'")[0]
-    issue.update(status="COMPLETE")
+    change_status(jira_client, issue, )
+
+
+def change_status(client: JIRA, issue: Issue, destination_status:str = "complete"):
+    transitions = client.transitions(issue)
+    transition_id = 0
+    for transition in transitions:
+        if destination_status in transition['name'].lower():
+            transition_id = transition['id']
+            break
+    client.transition_issue(issue, transition_id)
+
     
 
 if __name__ == "__main__":
-    pass # do nothing
+    # pass # do nothing
+    main()
